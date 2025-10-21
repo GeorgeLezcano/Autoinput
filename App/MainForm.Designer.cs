555aa71fc2c9a83849dc660072e3d18cc6036c36
george.lezcano <george.lezcano@ds2.com>
2025-10-21 14:00:43 -0500

    Added timer for runtime. Changed font and colors.

--- App/MainForm.Designer.cs
+++ App/MainForm.Designer.cs
@@ -28,48 +28,111 @@ partial class MainForm
     /// </summary>
     private void InitializeComponent()
     {
+        components = new System.ComponentModel.Container();
         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-        startButton = new Button();
-        stopButton = new Button();
+        startStopButton = new Button();
+        timerLabel = new Label();
+        runTimer = new System.Windows.Forms.Timer(components);
+        intervalLabel = new Label();
+        intervalInput = new NumericUpDown();
+        saveButton = new Button();
+        resetButton = new Button();
+        ((System.ComponentModel.ISupportInitialize)intervalInput).BeginInit();
         SuspendLayout();
         // 
-        // startButton
+        // startStopButton
         // 
-        startButton.Location = new Point(237, 388);
-        startButton.Name = "startButton";
-        startButton.Size = new Size(196, 79);
-        startButton.TabIndex = 0;
-        startButton.Text = "Start";
-        startButton.UseVisualStyleBackColor = true;
-        startButton.Click += StartButton_Click;
+        startStopButton.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        startStopButton.ForeColor = SystemColors.ButtonHighlight;
+        startStopButton.Location = new Point(145, 42);
+        startStopButton.Name = "startStopButton";
+        startStopButton.Size = new Size(196, 79);
+        startStopButton.TabIndex = 0;
+        startStopButton.Text = "Start";
+        startStopButton.UseVisualStyleBackColor = true;
+        startStopButton.Click += StartStopButton_Click;
         // 
-        // stopButton
+        // timerLabel
         // 
-        stopButton.Location = new Point(525, 388);
-        stopButton.Name = "stopButton";
-        stopButton.Size = new Size(196, 79);
-        stopButton.TabIndex = 1;
-        stopButton.Text = "Stop";
-        stopButton.UseVisualStyleBackColor = true;
-        stopButton.Click += StopButton_Click;
+        timerLabel.AutoSize = true;
+        timerLabel.Location = new Point(12, 518);
+        timerLabel.Name = "timerLabel";
+        timerLabel.Size = new Size(44, 20);
+        timerLabel.TabIndex = 1;
+        timerLabel.Text = "timer";
+        // 
+        // runTimer
+        // 
+        runTimer.Interval = 1000;
+        runTimer.Tick += RunTimer_Tick;
+        // 
+        // intervalLabel
+        // 
+        intervalLabel.AutoSize = true;
+        intervalLabel.Location = new Point(163, 145);
+        intervalLabel.Name = "intervalLabel";
+        intervalLabel.Size = new Size(154, 20);
+        intervalLabel.TabIndex = 4;
+        intervalLabel.Text = "Interval (Milliseconds)";
+        // 
+        // intervalInput
+        // 
+        intervalInput.Location = new Point(164, 168);
+        intervalInput.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+        intervalInput.Name = "intervalInput";
+        intervalInput.Size = new Size(150, 27);
+        intervalInput.TabIndex = 6;
+        intervalInput.Value = new decimal(new int[] { 1000, 0, 0, 0 });
+        // 
+        // saveButton
+        // 
+        saveButton.Location = new Point(185, 243);
+        saveButton.Name = "saveButton";
+        saveButton.Size = new Size(94, 29);
+        saveButton.TabIndex = 7;
+        saveButton.Text = "Save";
+        saveButton.UseVisualStyleBackColor = true;
+        saveButton.Click += SaveButton_Click;
+        // 
+        // resetButton
+        // 
+        resetButton.Location = new Point(185, 295);
+        resetButton.Name = "resetButton";
+        resetButton.Size = new Size(94, 29);
+        resetButton.TabIndex = 8;
+        resetButton.Text = "Reset";
+        resetButton.UseVisualStyleBackColor = true;
+        resetButton.Click += ResetButton_Click;
         // 
         // MainForm
         // 
         AutoScaleDimensions = new SizeF(8F, 20F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(961, 547);
-        Controls.Add(stopButton);
-        Controls.Add(startButton);
+        ClientSize = new Size(503, 547);
+        Controls.Add(resetButton);
+        Controls.Add(saveButton);
+        Controls.Add(intervalInput);
+        Controls.Add(intervalLabel);
+        Controls.Add(timerLabel);
+        Controls.Add(startStopButton);
         FormBorderStyle = FormBorderStyle.FixedDialog;
         Icon = (Icon)resources.GetObject("$this.Icon");
         MaximizeBox = false;
         Name = "MainForm";
         Text = "AutoInput";
+        Load += MainForm_Load;
+        ((System.ComponentModel.ISupportInitialize)intervalInput).EndInit();
         ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
 
-    private Button startButton;
-    private Button stopButton;
+    private Button startStopButton;
+    private Label timerLabel;
+    private System.Windows.Forms.Timer runTimer;
+    private Label intervalLabel;
+    private NumericUpDown intervalInput;
+    private Button saveButton;
+    private Button resetButton;
 }
--- App/MainForm.cs
+++ App/MainForm.cs
@@ -5,39 +5,80 @@ namespace App;
 /// </summary>
 public partial class MainForm : Form
 {
+    private bool isRunning = false;
+    private int activeTimerSeconds = 0;
+
     /// <summary>
     /// Default constructor.
     /// </summary>
     public MainForm()
     {
         InitializeComponent();
-        InitDefautState();
     }
 
     /// <summary>
-    /// Default state setup for initialization.
+    /// Initialization logic for first load.
     /// </summary>
-    private void InitDefautState()
+    private void MainForm_Load(object sender, EventArgs e)
     {
-        stopButton.Enabled = false;
+        SetTimeLabel(0);
+        startStopButton.BackColor = Color.Green;
     }
 
     /// <summary>
     /// Start button on click method.
     /// </summary>
-    private void StartButton_Click(object sender, EventArgs e)
+    private void StartStopButton_Click(object sender, EventArgs e)
     {
-        stopButton.Enabled = true;
-        startButton.Enabled = false;
+        isRunning = !isRunning;
+
+        (isRunning ? (Action)runTimer.Start : runTimer.Stop)();
+
+        startStopButton.Text = isRunning ? "Stop" : "Start";
+        saveButton.Enabled = !isRunning;
+        resetButton.Enabled = !isRunning;
+        intervalInput.Enabled = !isRunning;
+        startStopButton.BackColor = isRunning ? Color.Red : Color.Green;
     }
 
     /// <summary>
-    /// Stop button on click method.
+    /// Tick for run timer.
     /// </summary>
-    private void StopButton_Click(object sender, EventArgs e)
+    private void RunTimer_Tick(object sender, EventArgs e)
     {
-        startButton.Enabled = true;
-        stopButton.Enabled = false;
+        activeTimerSeconds++;
+        SetTimeLabel(activeTimerSeconds);
+    }
+
+    /// <summary>
+    /// Sets the time elapsed label text.
+    /// </summary>
+    /// <param name="seconds">The total time in seconds.</param>
+    private void SetTimeLabel(int seconds)
+    {
+        int hours = seconds / 3600;
+        int minutes = (seconds % 3600) / 60;
+        int secs = seconds % 60;
+
+        timerLabel.Text = $"Active Time: {hours:D2}:{minutes:D2}:{secs:D2}";
+    }
+
+    /// <summary>
+    /// Saves current configuration values.
+    /// </summary>
+    private void SaveButton_Click(Object sender, EventArgs e)
+    {
+        //TODO: Add save button logic.
+    }
+
+    /// <summary>
+    /// Resets all values to the default state.
+    /// </summary>
+    private void ResetButton_Click(Object sender, EventArgs e)
+    {
+        activeTimerSeconds = 0;
+        SetTimeLabel(activeTimerSeconds);
+        intervalInput.Value = 1000;
     }
 
 }
